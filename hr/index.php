f076807825fb380a3d0f19432e0ca3a16e9c3d1a
Richard Christian Villano <villanochristian23@gmai.com>
2018-02-04 06:20:03 +0800

    asdf

--- hr/index.php
+++ hr/index.php
@@ -1,7 +1,66 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Human Resource</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+	<link rel="stylesheet" type="text/css" href="css/mdb.css" />
+	<link rel="stylesheet" type="text/css" href="css/font-awesome.css" />
+	<link rel="stylesheet" type="text/css" href="css/style.css" />
+</head>
+<body>
+                   
+<!--Navbar-->
+<nav class="navbar navbar-expand-lg navbar-dark indigo">
 
+    <!-- Navbar brand -->
+    <a class="navbar-brand" href="#">Chicken Dinner</a>
 
+    <!-- Collapse button -->
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+        aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
 
+    <!-- Collapsible content -->
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
+        <!-- Links -->
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Features</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Pricing</a>
+            </li>
 
-?>
+            <!-- Dropdown -->
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Manage Employee</a>
+                <div class="dropdown-menu dropdown-primary" aria-labelledby="navbarDropdownMenuLink">
+                    <a class="dropdown-item" href="#">Add Employee</a>
+                    <a class="dropdown-item" href="#">Employed</a>
+                    <a class="dropdown-item" href="#">Unemployed</a>
+                    <a class="dropdown-item" href="#">Employee Logs</a>
+                </div>
+            </li>
+
+        </ul>
+        <!-- Links -->
+
+        <!-- Search form -->
+        <form class="form-inline">
+            <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+        </form>
+    </div>
+    <!-- Collapsible content -->
+
+</nav>
+<!--/.Navbar-->
+    <script type="text/javascript" src="js/mdb/jquery-3.2.1.min.js"></script>
+	<script type="text/javascript" src="js/mdb/mdb.js"></script>
+	<script type="text/javascript" src="js/mdb/bootstrap.js"></script>
+	<script type="text/javascript" src="js/mdb/popper.min.js"></script>
+	<!-- <script type="text/javascript" src="js/login.js"></script> -->
+</body>
+</html>
